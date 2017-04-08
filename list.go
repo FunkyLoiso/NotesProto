bcb10e7ff67ec3ad52e7c6a6ed3015b445dd69e1
FunkyLoiso <nein@apologist>
2017-04-08 22:52:46 +0300

    command differentiation added

---
+++ list.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+)
+
+func list() error {
+	log.Println("starting list command")
+
+	return nil
+}
--- main.go
+++ main.go
@@ -4,28 +4,66 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 )
 
-func main() {
-	fmt.Println("Notes proto be here")
+var cfg config
 
-	lfile, err := os.OpenFile("./NotesProto.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+type commandInfo struct {
+	execute     func() error
+	description string
+}
+
+var commands = map[string]commandInfo{
+	"list": {list, "List notes from a notepad, newest first"},
+}
+
+func printHelp() {
+	appPath, err := os.Executable()
+	if err != nil {
+		log.Printf("os.Executable failed: %v", err)
+		appPath = os.Args[0]
+	}
+	fmt.Printf("%v - somewhat potentially ok notes manager.\nUsage:\n", path.Base(appPath))
+	for cmd, info := range commands {
+		fmt.Printf("%v\t%v\n", cmd, info.description)
+	}
+}
+
+func main() {
+	lFile, err := os.OpenFile("./NotesProto.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Failed to open log file")
 	} else {
-		log.SetOutput(lfile)
+		log.SetOutput(lFile)
 		log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
 	}
 
 	log.Println("==================== NotesProto new log entry ====================")
+	log.Printf("starting with args: %v\n", os.Args)
 
-	var conf config
-	err = conf.read()
-	fmt.Println(err)
+	err = cfg.read()
+	if err != nil {
+		log.Println("Error reading config file: %v", err)
+	}
 
-	conf.Source = "local"
+	var (
+		cmd   commandInfo
+		found bool
+	)
+	if len(os.Args) > 1 {
+		cmd, found = commands[os.Args[1]]
+	} else {
+		found = false
+	}
+	if !found {
+		printHelp()
+	} else {
+		err = cmd.execute()
+		if err != nil {
+			log.Printf("Error while executing command '%v': %v", os.Args[1], err)
+		}
+	}
 
-	err = conf.write()
-	fmt.Println(err)
 	log.Println("NotesProto stop")
 }
